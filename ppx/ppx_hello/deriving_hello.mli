83dfebd5ff501a7720920bbf4feb677cad39f2e4
Ernest Ng <ngernest@sas.upenn.edu>
2023-03-15 14:39:28 -0400

    added ppx_hello example

---
+++ ppx/ppx_hello/deriving_hello.mli
@@ -0,0 +1,12 @@
+val module_name_of_type : Ppxlib.type_declaration -> string Location.loc
+
+val str_gen :
+  loc:Warnings.loc ->
+  path:string ->
+  'a * Ppxlib.type_declaration list -> Ppxlib.structure_item list
+
+val sig_gen :
+  loc:Warnings.loc ->
+  path:'a -> 'b * Ppxlib.type_declaration list -> Ppxlib.signature_item list
+
+  val name : string
