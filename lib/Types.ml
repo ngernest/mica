0b5a76e191bb3e4caeb34661b75c43fa66021898
Ernest Ng <ngernest@sas.upenn.edu>
2023-04-20 17:14:06 -0400

    Second attempt at implementing API for sets

--- lib/Types.ml
+++ lib/Types.ml
@@ -1,36 +1,71 @@
 open Core
-open Core.Quickcheck 
+open Base_quickcheck 
 open Sets
 
-(** ADT representing types, T1 & T2 are the abstract types of the two Set implementations *)
-(** TODO: not sure how to associate T1 & T2 with the actual implementations 
-    -- do we need a function mapping each Ti to the appropriate module satisfying SetIntf? *)
-type ty = T1 | T2 | Int of int 
 
-(** Type representing values, i.e. observations made *)
-type value = IntV of int | BoolV of bool 
-
-(** Commands that change the underlying state *)
+(** Revised definition of expressions *)
 type expr = 
+  (* Commands that change the underlying state *)
   | Add of int * expr
   | Remove of int * expr
   | Union of expr * expr 
   | Intersection of expr * expr 
   | Empty
-  [@@deriving sexp_of, quickcheck]
-  
-(** Observations that don't change the underlying state *)
-type observable = 
+  (* Observable commands *)
   | Mem of int * expr 
   | Size of expr
   | Is_empty of expr  
   [@@deriving sexp_of, quickcheck]
 
-(** Generator for expressions?  *)  
-let gen_exp (t : ty) : expr Generator.t = failwith "TODO"
+(** ADT representing types, where [T] is the abstract type of the module *)
+type ty = T | Int | Bool
 
-(** Interprets the symbolic command over the model / SUT *)
-let interp_expr (expr : expr) : expr = failwith "TODO"  
 
-(** Interprets observations over the model / SUT *)
-let interp_obs (obs: observable) : value = failwith "TODO"
+(** Functor relating [expr] to the actual module [M] implementing [SetIntf] *)
+module ExprToImpl (M : SetIntf) = struct 
+  
+  (** Type representing values, i.e. observations made *)
+  type value = ValT of int M.t | ValInt of int | ValBool of bool
+
+
+  (** TODO: find a way to extract the abstract type [T] from the [expr] 
+       similar to "bind" in Haskell ?? *)
+  let extract (e : expr) : int M.t = failwith "TODO"
+
+  (** [interp expr] interprets the expression [expr] wrt the module 
+      implementation [M], and returns the result as a [value] *)
+  let interp (expr : expr) : value =
+    match expr with 
+    | Add (x, s) -> ValT (M.add x (extract s))
+    | Remove (x, s) -> ValT (M.rem x (extract s))
+    | Union (s1, s2) -> ValT (M.union (extract s1) (extract s2))
+    | Intersection (s1, s2) -> ValT (M.inter (extract s1) (extract s2))
+    | Empty -> ValT M.empty
+    | Mem (x, s) -> ValBool (M.mem x (extract s))
+    | Size s -> ValInt (M.size (extract s))
+    | Is_empty e -> ValBool (M.is_empty (extract s))
+end
+
+
+
+(** Generator for expressions *)  
+let gen_expr (ty : ty) : expr Generator.t = 
+  let module M = ListSetNoDups in 
+  let s = Empty in 
+  let module G = Generator in 
+  match ty with 
+  (** TODO: how to generate [T]'s ?? *)
+  | T -> G.(union @@ 
+    [ small_positive_or_zero_int >>= (fun x -> return @@ Add (x, s));
+      small_positive_or_zero_int >>= (fun x -> return @@ Remove (x, s));
+      return Empty
+    ])
+  | Int -> G.return @@ Size s
+  | Bool -> G.(union @@ 
+    [ small_positive_or_zero_int >>= (fun x -> return @@ Mem (x, s));
+      return @@ Is_empty s
+    ])
+    
+
+
+
