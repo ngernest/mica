c2a7bcd728b0e1a91b2d5ad841965770836253cb
Ernest Ng <ngernest@sas.upenn.edu>
2023-07-23 16:59:19 +0800

    Made QC generators part of the ECDH signature

--- x25519/ECDHIntf.mli
+++ x25519/ECDHIntf.mli
@@ -1,9 +1,8 @@
-open Base_quickcheck
 module type ECDHIntf = sig
   type public_key  [@@deriving sexp_of]
   type private_key [@@deriving sexp_of]
-  val quickcheck_generator_public_key : public_key Generator.t
-  val quickcheck_generator_private_key : private_key Generator.t
+  val quickcheck_generator_public_key : public_key Base_quickcheck.Generator.t
+  val quickcheck_generator_private_key : private_key Base_quickcheck.Generator.t
   val base : public_key
   val public_key_of_string : string -> public_key
   val private_key_of_string : string -> private_key
