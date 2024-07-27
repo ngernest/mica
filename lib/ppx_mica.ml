124e3d2608c62d8d52d50e334df7fbf04f35cfed
Ernest Ng <ernest@perpay.com>
2024-07-27 12:50:01 -0400

    Allow users to just write [@@deriving mica] in lieu of multiple directives

--- lib/ppx_mica.ml
+++ lib/ppx_mica.ml
@@ -13,13 +13,16 @@ let args () =
 
 (** Registers the main [mica_types] and [mica] PPX derivers *)
 let () =
-  List.iter ~f:Reserved_namespaces.reserve [ "mica_types"; "mica" ];
+  List.iter ~f:Reserved_namespaces.reserve
+    [ "mica_types"; "mica_functor"; "mica" ];
   (* Generate auxiliary type declarations *)
   let datatype_deriver =
     Deriving.add "mica_types" ~str_module_type_decl:type_generator in
   (* Generate the body of the [TestHarness] functor - Note that we must generate
      the declarations of auxiliary datatypes before generating the functor *)
   let functor_generator =
-    Deriving.Generator.V2.make_noarg ~deps:[ datatype_deriver ]
-      Functor_deriver.generate_functor in
-  Deriving.add "mica" ~str_module_type_decl:functor_generator |> Deriving.ignore
+    Deriving.Generator.V2.make_noarg Functor_deriver.generate_functor in
+  let functor_deriver =
+    Deriving.add "mica_functor" ~str_module_type_decl:functor_generator in
+  Deriving.add_alias "mica" [ datatype_deriver; functor_deriver ]
+  |> Deriving.ignore
