8196235ffc4e0a287e0da4d55a7ec17aaceecfb0
Ernest Ng <ernest@perpay.com>
2024-08-01 14:05:51 -0400

    Use Metaquot to produce generated code in type_deriver

--- lib/type_deriver.ml
+++ lib/type_deriver.ml
@@ -293,10 +293,11 @@ let generate_types_from_sig ~(ctxt : Expansion_context.Deriver.t)
         let ty_td = mk_adt ~loc ~name:"ty" ~cstrs:ty_cstrs in
         let annotated_ty_td =
           { ty_td with ptype_attributes = [ deriving_show ~loc ] } in
-        [ pstr_type ~loc Recursive [ annotated_expr_td ];
-          pstr_type ~loc Recursive [ annotated_ty_td ];
-          [%stri let rec gen_expr ty = [%e derive_gen_expr ~loc sig_items]]
-        ])
+        [%str
+          [%%i pstr_type ~loc Recursive [ annotated_expr_td ]]
+          [%%i pstr_type ~loc Recursive [ annotated_ty_td ]]
+
+          let rec gen_expr ty = [%e derive_gen_expr ~loc sig_items]])
     | _ -> failwith "TODO: other case for mod_type")
   | { pmtd_type = None; pmtd_loc; _ } ->
     [ mk_error_pstr ~local:pmtd_loc ~global:loc
